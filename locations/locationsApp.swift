29473308901c39f2221534c5549408fa312771e4
Saba Khutsishvili <sabakhuts@gmail.com>
2021-08-28 15:48:59 +0400

    Initial Commit

---
+++ locations/locationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  locationsApp.swift
+//  locations
+//
+//  Created by Saba Khutsishvili on 28.08.21.
+//
+
+import SwiftUI
+
+@main
+struct locationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
