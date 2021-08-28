7d77045da642abd8d26ad928b80ce101ad0503d5
Saba Khutsishvili <sabakhuts@gmail.com>
2021-08-28 18:54:47 +0400

    Add `LandmarkRow`

---
+++ locations/Views/LandmarkRow.swift
@@ -0,0 +1,39 @@
+//
+//  LandmarkRow.swift
+//  locations
+//
+//  Created by Saba Khutsishvili on 28.08.21.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+    
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        Group {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+            LandmarkRow(landmark: landmarks[2])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+    }
+    
+}
