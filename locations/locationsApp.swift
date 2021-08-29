deb6fbd77d3161d2216f34e8da0c13f05357c82b
Saba Khutsishvili <sabakhuts@gmail.com>
2021-08-29 08:24:29 +0400

    Add  as SOT

--- locations/locationsApp.swift
+++ locations/locationsApp.swift
@@ -10,9 +10,12 @@ import SwiftUI
 @main
 struct LocationsApp: App {
     
+    @StateObject private var modelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
     }
     
