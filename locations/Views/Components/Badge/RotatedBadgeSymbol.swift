f49e549c85867ef8fef1b47ecce116867ddf1734
Saba Khutsishvili <sabakhuts@gmail.com>
2021-08-29 14:09:11 +0400

    Add rotate badge symbol view

---
+++ locations/Views/Components/Badge/RotatedBadgeSymbol.swift
@@ -0,0 +1,28 @@
+//
+//  RotatedBadgeSymbol.swift
+//  locations
+//
+//  Created by Saba Khutsishvili on 29.08.21.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+    
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+    
+}
