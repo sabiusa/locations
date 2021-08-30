2a0f4ed679db38c2ee1425e1621c3d6ce1061e3e
Saba Khutsishvili <sabakhuts@gmail.com>
2021-08-30 18:26:19 +0400

    Add `FeatureCard`

---
+++ locations/Views/FeatureCard.swift
@@ -0,0 +1,28 @@
+//
+//  FeatureCard.swift
+//  locations
+//
+//  Created by Saba Khutsishvili on 30.08.21.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    
+    var landmark: Landmark
+    
+    var body: some View {
+        landmark.featuredImage?
+            .resizable()
+            .aspectRatio(3 / 2, contentMode: .fit)
+    }
+    
+}
+
+struct FeatureCard_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        FeatureCard(landmark: ModelData().features[0])
+    }
+    
+}
