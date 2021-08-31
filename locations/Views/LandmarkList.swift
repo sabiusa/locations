fb69d38a3c1e5ea8ebe1ad2f1a08e74d22326ed1
Saba Khutsishvili <sabakhuts@gmail.com>
2021-08-31 13:59:56 +0400

    Make filtering work on mac and ios

--- locations/Views/LandmarkList.swift
+++ locations/Views/LandmarkList.swift
@@ -10,30 +10,65 @@ import SwiftUI
 struct LandmarkList: View {
     
     @State private var showFavoritesOnly = false
+    @State private var filter = FilterCategory.all
     
     @EnvironmentObject var modelData: ModelData
     
+    enum FilterCategory: String, CaseIterable, Identifiable {
+        case all = "All"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+
+        var id: FilterCategory { self }
+    }
+    
     var filteredLandmarks: [Landmark] {
         modelData.landmarks.filter { landmark in
-            (!showFavoritesOnly || landmark.isFavorite)
+            (!showFavoritesOnly || landmark.isFavorite) &&
+                (filter == .all || filter.rawValue == landmark.category.rawValue)
         }
+    }
+    
+    var title: String {
+        let title = filter == .all ? "Landmarks" : filter.rawValue
+        return showFavoritesOnly ? "Favorite \(title)" : title
     }
     
     var body: some View {
         NavigationView {
             List {
-                Toggle(isOn: $showFavoritesOnly) {
-                    Text("Favorites only")
-                }
-                
                 ForEach(filteredLandmarks) { landmark in
                     NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
                         LandmarkRow(landmark: landmark)
                     }
                 }
             }
-            .navigationTitle("Landmarks")
+            .navigationTitle(title)
             .frame(minWidth: 300)
+            .toolbar {
+                ToolbarItem {
+                    Menu(
+                        content: {
+                            Picker("Category", selection: $filter) {
+                                ForEach(FilterCategory.allCases) { category in
+                                    Text(category.rawValue).tag(category)
+                                }
+                            }
+                            .pickerStyle(InlinePickerStyle())
+                            
+                            Toggle(isOn: $showFavoritesOnly) {
+                                Label("Favorites only", systemImage: "star.fill")
+                            }
+                        },
+                        label: {
+                            Label("Filter", systemImage: "slider.horizontal.3")
+                        }
+                    )
+                }
+            }
+            
+            Text("Select a Landmark")
         }
     }
     
