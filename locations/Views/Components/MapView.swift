c4bc72360a12bd69f3bc61626b4b31c36f7eb8cd
Saba Khutsishvili <sabakhuts@gmail.com>
2021-08-31 14:38:30 +0400

    Add ability to zoom in/out on map region

--- locations/Views/Components/MapView.swift
+++ locations/Views/Components/MapView.swift
@@ -14,17 +14,36 @@ struct MapView: View {
     
     var coordinate: CLLocationCoordinate2D
     
-    var body: some View {
-        Map(coordinateRegion: $region)
-            .onAppear {
-                setRegion(coordinate)
-            }
+    @AppStorage("MapView.zoom")
+    private var zoom = Zoom.medium
+    
+    enum Zoom: String, CaseIterable, Identifiable {
+        case near = "Near"
+        case medium = "Medium"
+        case far = "Far"
+
+        var id: Zoom { self }
     }
     
-    private func setRegion(_ coordinate: CLLocationCoordinate2D) {
-        region = MKCoordinateRegion(
+    var delta: CLLocationDegrees {
+        switch zoom {
+        case .near: return 0.02
+        case .medium: return 0.2
+        case .far: return 2
+        }
+    }
+    
+    var body: some View {
+        Map(coordinateRegion: .constant(region))
+    }
+    
+    var regions: MKCoordinateRegion {
+        return MKCoordinateRegion(
             center: coordinate,
-            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+            span: MKCoordinateSpan(
+                latitudeDelta: delta,
+                longitudeDelta: delta
+            )
         )
     }
     
@@ -33,7 +52,13 @@ struct MapView: View {
 struct MapView_Previews: PreviewProvider {
     
     static var previews: some View {
-        MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
+        MapView(
+            coordinate:
+                CLLocationCoordinate2D(
+                    latitude: 34.011_286,
+                    longitude: -116.166_868
+                )
+        )
     }
     
 }
