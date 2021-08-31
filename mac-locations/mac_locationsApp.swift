2dc3e7629cabe435b2a032cf6f907c3639d28abc
Saba Khutsishvili <sabakhuts@gmail.com>
2021-08-31 12:30:32 +0400

    Add mac target

---
+++ mac-locations/mac_locationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_locationsApp.swift
+//  mac-locations
+//
+//  Created by Saba Khutsishvili on 31.08.21.
+//
+
+import SwiftUI
+
+@main
+struct mac_locationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
