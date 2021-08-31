b21849b0de29984db58a7d2ce11d7408f5321595
Saba Khutsishvili <sabakhuts@gmail.com>
2021-08-31 08:23:54 +0400

    Add watch landmark details

--- watch-locations Extension/Views/LandmarkDetail.swift
+++ watch-locations Extension/Views/LandmarkDetail.swift
@@ -18,7 +18,39 @@ struct LandmarkDetail: View {
     }
     
     var body: some View {
-        Text("Text")
+        ScrollView {
+            VStack {
+                CircleImage(image: landmark.image.resizable())
+                    .scaledToFit()
+                
+                Text(landmark.name)
+                    .font(.headline)
+                    .lineLimit(0)
+                
+                Toggle(isOn: $modelData.landmarks[landmarkIndex].isFavorite) {
+                    Text("Favorite")
+                }
+                
+                Divider()
+                
+                Text(landmark.park)
+                    .font(.caption)
+                    .bold()
+                    .lineLimit(0)
+                
+                Text(landmark.state)
+                    .font(.caption)
+                    .lineLimit(0)
+                
+                Divider()
+                
+                MapView(coordinate: landmark.locationCoordinate)
+                    .scaledToFit()
+                    .cornerRadius(10)
+            }
+            .padding(16)
+        }
+        .navigationTitle("Landmarks")
     }
     
 }
@@ -28,8 +60,15 @@ struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
         let modelData = ModelData()
         
-        LandmarkDetail(landmark: modelData.landmarks[0])
-            .environmentObject(modelData)
+        return Group {
+            LandmarkDetail(landmark: modelData.landmarks[0])
+                .environmentObject(modelData)
+                .previewDevice("Apple Watch Series 6 - 44mm")
+            
+            LandmarkDetail(landmark: modelData.landmarks[0])
+                .environmentObject(modelData)
+                .previewDevice("Apple Watch Series 6 - 40mm")
+        }
     }
     
 }
